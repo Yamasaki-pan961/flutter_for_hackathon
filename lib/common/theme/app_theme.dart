dcb05c53f5f92f21398d17f2e9f5ed1d790a8208
iseruuuuu <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>
2023-10-01 04:54:08 +0900

    fix: static const

--- lib/common/theme/app_theme.dart
+++ lib/common/theme/app_theme.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static Color appColor = const Color(0xFF72F1B7);
-  static Color backgroundColor = const Color(0xFF1E1E20);
-  static Color textColor = Colors.white;
-  static Color unSelectedColor = const Color(0xFF606060);
-  static Color shade700Color = const Color(0xFF404040);
-  static Color grey = Colors.grey;
-  static Color buttonStart = const Color(0xFF00FF8B);
-  static Color buttonEnd = const Color(0xFF3B8E68);
+  static const Color appColor = Color(0xFF72F1B7);
+  static const Color backgroundColor =  Color(0xFF1E1E20);
+  static const Color textColor = Colors.white;
+  static const Color unSelectedColor =  Color(0xFF606060);
+  static const Color shade700Color =  Color(0xFF404040);
+  static const Color grey = Colors.grey;
+  static const Color buttonStart = Color(0xFF11A05F);
+  static const Color buttonEnd = Color(0xFF00FF8B);
 }
