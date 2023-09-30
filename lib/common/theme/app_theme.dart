a66747b4b07b65848cc882b692419fd6a9081390
iseruuuuu <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>
2023-09-30 21:33:41 +0900

    add: app_themeを追加しました

---
+++ lib/common/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static Color appColor = const Color(0xFF72F1B7);
+  static Color backgroundColor = Colors.black;
+  static Color textColor = Colors.white;
+  static Color unSelectedColor = const Color(0xFF606060);
+  static Color shade700Color = Colors.grey.shade700;
+  static Color grey = Colors.grey;
+}
