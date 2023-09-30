69e91711f8cd113099243b7d431fa943246835f7
iseruuuuu <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>
2023-09-30 22:32:34 +0900

    add: spacesを追加しました

--- lib/common/theme/app_spaces.dart
+++ lib/common/theme/app_spaces.dart
@@ -20,6 +20,7 @@ class AppSpaces {
   static const SizedBox vertical_24 = SizedBox(height: 24);
   static const SizedBox vertical_30 = SizedBox(height: 30);
   static const SizedBox vertical_40 = SizedBox(height: 40);
+  static const SizedBox vertical_50 = SizedBox(height: 50);
   static const SizedBox vertical_80 = SizedBox(height: 80);
   static const SizedBox vertical_82 = SizedBox(height: 82);
   static const SizedBox vertical_100 = SizedBox(height: 100);
